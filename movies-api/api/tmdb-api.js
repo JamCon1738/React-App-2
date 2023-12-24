caa5da9b85090a05718abd48fbb05346498ea9b8
JamCon1738 <jamieconlon1738@gmail.com>
2023-12-24 21:54:49 +0000

    Basic Integration

--- movies-api/api/tmdb-api.js
+++ movies-api/api/tmdb-api.js
@@ -30,3 +30,45 @@ export const getGenres = async () => {
         throw error;
     }
 };
+
+
+export const getMovies = async () => {
+    try {
+        const response = await fetch(
+            `https://api.themoviedb.org/3/discover/movie?api_key=${process.env.REACT_APP_TMDB_KEY}&language=en-US&include_adult=false&include_video=false&page=1`
+             );
+
+        if (!response.ok) {
+            throw new Error(response.json().message);
+        }
+    return await response.json();
+} catch (error) {
+    throw error;
+}
+};
+
+
+export const getMovie = async (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}?api_key=${process.env.TMDB_KEY}`
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.json().message);
+      }
+      return response.json();
+    })
+    .catch((error) => {
+      throw error
+  });
+  };
+
+
+  export const getMovieReviews = async (id) => {
+    return fetch(
+      `https://api.themoviedb.org/3/movie/${id}/reviews?api_key=${process.env.TMDB_KEY}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        return json.results;
+      });
+  };  
